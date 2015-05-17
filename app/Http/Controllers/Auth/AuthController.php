bb9cc14c5af5d2e228e553403e02c98d06f50c0f
Manny Salom <manny.salom@gmail.com>
2015-05-17 12:50:29 -0400

    Updated the Auth class

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -20,6 +20,8 @@ class AuthController extends Controller {
 
 	use AuthenticatesAndRegistersUsers;
 
+	protected $redirectTo = 'notices/create';
+
 	/**
 	 * Create a new authentication controller instance.
 	 *
