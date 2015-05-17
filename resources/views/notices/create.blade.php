abddb5e867e71413a2657e9d8c42d0e1f69a1966
Manny Salom <manny.salom@gmail.com>
2015-05-17 14:00:59 -0400

    Add the form to create a new mcda notice

--- resources/views/notices/create.blade.php
+++ resources/views/notices/create.blade.php
@@ -1,5 +1,33 @@
 @extends('app')
 
 @section('content')
-	Create a new notice
+	<h1 class="page-heading">Prepare DMCA Notice</h1>
+	{!! Form::open() !!}
+
+		<div class="form-group">
+			{!! Form::label('provider_id', 'Who are we sending this to?') !!}
+			{!! Form::select('provider_id', [], null, ['class' => 'form-control']) !!}
+		</div>
+		<div class="form-group">
+			{!! Form::label('infringing_title', 'Infringing_title:') !!}
+			{!! Form::text('infringing_title', null, ['class' => 'form-control']) !!}
+		</div>
+
+		<div class="form-group">
+			{!! Form::label('infringing_link', 'Infringing_link:') !!}
+			{!! Form::text('infringing_link', null, ['class' => 'form-control']) !!}
+		</div>
+		<div class="form-group">
+			{!! Form::label('original_link', 'Original_link:') !!}
+			{!! Form::text('original_link', null, ['class' => 'form-control']) !!}
+		</div>
+		<div class="form-group">
+			{!! Form::label('original_description', 'Original_description:') !!}
+			{!! Form::text('original_description', null, ['class' => 'form-control']) !!}
+		</div>
+		<div class="form-group">
+			{!! Form::submit('Preview Notice', ['class' => 'btn btn-primary form-control']) !!}
+		</div>
+
+	{!! Form::close() !!}
 @endsection
